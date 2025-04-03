33717585c65177049a389abc11d8306c17f4219d
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-04-03 21:04:42 +0330

    design UI for edit and remove icons

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import { useRef } from "react";
 import supabase from "../../../supabase-client";
 import { useEffect } from "react";
 import useFetch from "../../../useFetch";
+import { MdDelete } from "react-icons/md";
+import { MdModeEdit } from "react-icons/md";
 
 const Home = () => {
   //disable_eslint_next_line
@@ -20,41 +22,43 @@ const Home = () => {
     setNewTask(valueItem.current.value);
   };
 
-  useFetch();
-  useEffect(() => {
-    console.log(toDoList);
-    const readTasks = async () => {
-      const { data, error } = await supabase.from("ToDoList").select("*");
-      data ? setToDoList(data) : console.log("Error into ReadTask", error);
-    };
-    readTasks();
-    const addTask = async () => {
-      console.log(newTask);
-      const objNewTask = {
-        name: newTask,
-        isCompleted: false,
-      };
-      if (newTask) {
-        const { data, error } = await supabase
-          .from("ToDoList")
-          .insert(objNewTask)
-          .single();
-        error ? console.log("Error", error) : toDoList.push(objNewTask);
-      }
-    };
-    addTask();
-  }, [newTask]);
-
+  useFetch(toDoList, supabase, newTask, setToDoList);
   return (
     <>
-      <div className="home">
-        <input type="text" name="" ref={valueItem} />
-        <button type="button" onClick={addTaskToHook}>
-          Befehl erstellen
-        </button>
-        <div className="result_Box flex justify-around">
+      <div className="home mx-auto text-center ">
+        <div className="  text-center mx-auto ">
+          <div className="flex flex-col mx-auto justify-center w-[50rem] tex-center items-center">
+            <input
+              type="text"
+              name=""
+              ref={valueItem}
+              className="bg-amber-50 p-8 w-[45rem]"
+            />
+            <button
+              type="button"
+              onClick={addTaskToHook}
+              className="bg-cyan-500"
+            >
+              Aufgabe erstellen
+            </button>
+          </div>
+        </div>
+        <div className="result_Box flex flex-col gap-4 items-baseline justify-center">
           {toDoList.map((i) => {
-            return <p key={i.id}>{i.name}</p>;
+            return (
+              <div
+                key={i.id}
+                className="bg-stone-400  min-w-64 px-4 rounded-2xl  mx-auto"
+              >
+                <p className="flex flex-row justify-between items-center ">
+                  {i.name}
+                  <div className="iconsManager flex justify-center items-center w-[30%]">
+                    <MdDelete className="cursor-pointer" />{" "}
+                    <MdModeEdit className="cursor-pointer" />
+                  </div>
+                </p>
+              </div>
+            );
           })}
         </div>
       </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,4 +1,27 @@
-const useFetch = () => {
-  console.log("Hi");
+import { useEffect } from "react";
+const useFetch = (toDoList, supabase, newTask, setToDoList) => {
+  useEffect(() => {
+    console.log(toDoList);
+    const readTasks = async () => {
+      const { data, error } = await supabase.from("ToDoList").select("*");
+      data ? setToDoList(data) : console.log("Error into ReadTask", error);
+    };
+    readTasks();
+    const addTask = async () => {
+      console.log(newTask);
+      const objNewTask = {
+        name: newTask,
+        isCompleted: false,
+      };
+      if (newTask) {
+        const { data, error } = await supabase
+          .from("ToDoList")
+          .insert(objNewTask)
+          .single();
+        error ? console.log("Error", error) : toDoList.push(objNewTask);
+      }
+    };
+    addTask();
+  }, [newTask]);
 };
 export default useFetch;
